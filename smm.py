1b617b445d5939925a668c366d880fb5c5298130
trivuniceva <trivunic99@gmail.com>
2024-06-16 04:26:11 +0200

    Update smm.py
    R^2: 1.00
    MAE: 3651174.03
    MSE: 461575774665931.25

--- smm.py
+++ smm.py
@@ -1,43 +1,37 @@
 import pandas as pd
-from sklearn.preprocessing import StandardScaler
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split, RandomizedSearchCV
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
 
 data_path = '/Users/nikolina/Desktop/Projekti/ml_tiktok_data/metadata.csv'
+# data_path = 'small_data.csv'
+
 df = pd.read_csv(data_path)
 
 columns_to_drop = ['secretID', 'covers.default', 'covers.origin', 'covers.dynamic', 
                    'webVideoUrl', 'videoUrl', 'videoUrlNoWaterMark', 'videoApiUrlNoWaterMark']
-df_cleaned = df.drop(columns=columns_to_drop)
-df_cleaned['createTime'] = pd.to_datetime(df_cleaned['createTime'], unit='s')
+df = df.drop(columns=columns_to_drop)
 
-df_cleaned['days_since_creation'] = (pd.Timestamp.now() - df_cleaned['createTime']).dt.days
-df_cleaned['engagement_rate'] = df_cleaned['authorMeta.heart'] / df_cleaned['authorMeta.video']
+df['createTime'] = pd.to_datetime(df['createTime'], unit='s')
 
-scaler = StandardScaler()
+df['days_since_creation'] = (pd.Timestamp.now() - df['createTime']).dt.days
+df['engagement_rate'] = df['authorMeta.heart'] / df['authorMeta.video']
+
+vectorizer = TfidfVectorizer()
+text_features = vectorizer.fit_transform(df['text'].fillna('').values.astype('U'))
+df_text_features = pd.DataFrame(text_features.toarray(), columns=vectorizer.get_feature_names_out())
+
+df_cleaned = pd.concat([df.drop(columns=['text']), df_text_features], axis=1)
+
+scaler = MinMaxScaler()
 columns_to_scale = ['days_since_creation', 'engagement_rate']
 df_cleaned[columns_to_scale] = scaler.fit_transform(df_cleaned[columns_to_scale])
 
-vectorizer = TfidfVectorizer()
-text_features = vectorizer.fit_transform(df_cleaned['text'].values.astype('U'))
-df_text_features = pd.DataFrame(text_features.toarray(), columns=vectorizer.get_feature_names_out())
-df_cleaned_with_text_features = pd.concat([df_cleaned, df_text_features], axis=1)
-
-df_cleaned_numeric = df_cleaned_with_text_features.select_dtypes(include=[float, int])
-
-correlation_matrix = df_cleaned_numeric.corr()
-print(correlation_matrix)
-
-print(df_cleaned[['authorMeta.fans', 'authorMeta.heart', 'authorMeta.video']].describe())
-
-target_variable = 'authorMeta.heart'
-
-print("Treniranje modela...")
-
-X = df_cleaned_numeric.drop(columns=[target_variable])
-y = df_cleaned_numeric[target_variable]
+numeric_columns = df_cleaned.select_dtypes(include=['float64', 'int64']).columns
+X = df_cleaned[numeric_columns].drop(columns=['authorMeta.heart'])  # X su sve karakteristike osim ciljne promenljive
+y = df_cleaned['authorMeta.heart']  # Y je ciljna promenljiva
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
